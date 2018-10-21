c6c814f947d1996774d89ff4901ab10f7ec37a4c
Abhishek Singh <singhabhishek.singh@gmail.com>
2018-10-21 11:26:00 +0530

    This is my program

---
+++ exe13.py
@@ -0,0 +1,6 @@
+from sys import argv
+script, first, second, third = argv
+print('This Script is called script', script)
+print('This is the first variable', first)
+print('This is the second variable', second)
+print('This is the third variable', third)
